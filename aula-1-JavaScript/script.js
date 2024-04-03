4a9589fde28f0b856d3228a7e85976f87fb69c25
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-04-03 20:56:53 -0300

    primeira aula de js

---
+++ aula-1-JavaScript/script.js
@@ -0,0 +1,14 @@
+/*alert("Olá, Mundo!")
+
+prompt("Digite seu nome:")
+
+confirm("Você deseja sair?")*/
+
+//entrada
+var nome = prompt("Digite seu nome:");
+var idade = prompt("Digite sua idade");
+var profissao = prompt("Qual sua profissão?");
+
+//saída
+//console.log("Olá " + nome + ", seja bem-vindo!");
+console.log(`Olá ${nome}, seja bem vindo! Feliz em saber que você tem ${idade} anos e trabalha com ${profissao}`);
