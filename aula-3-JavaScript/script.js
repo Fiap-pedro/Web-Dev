f3ae26b27cc15fb3d3bae90806c2786c8bd66aa9
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-04-24 21:01:20 -0300

    aula 4

--- aula-3-JavaScript/script.js
+++ aula-3-JavaScript/script.js
@@ -1,18 +1,20 @@
  //ENTRADA
-
- //PROCESSAMENTO
  function calcular() {
-    var qtd = parseInt(document.getElementById('qtd').value)
-    if(qtd >= 7 ){
-        preco = qtd * 15
-        alert(`O preço dos ${qtd} livros fica R$${preco}`)
+    var quantidade = parseInt(document.getElementById('quantidade').value)
+    var resultado = document.getElementById('resultado')
+ //PROCESSAMENTO
+ 
+    if(quantidade >= 7 ){
+        preco = quantidade * 15
     }
     else{
-        preco = qtd * 22
-        alert(`O preço dos ${qtd} livros fica R$${preco}`)
+        preco = quantidade * 22
     }
+
+//SAÍDA
+    resultado.innerText = `O preço dos ${quantidade} livros fica R$${preco}`
 }
 
 
 
- //SAÍDA
+ 
---
+++ aula-4-JavaScript/script.js
@@ -0,0 +1,40 @@
+// //ENTRADA
+// let senha
+// let nome
+
+// function logar() {
+    
+// //PROCESSAMENTO
+
+//     while (true) {
+//         nome = document.getElementById('nome').value
+//         senha = document.getElementById('senha').value
+//         if (nome && nome.trim().length > 0 && senha == '1234') {
+//             alert('Logado.')
+//             break;
+//         }
+//         else {
+//             alert('Nome e senha inválidos, digite um nome válido, por favor.')
+//         }
+
+    
+
+    
+
+// }
+
+// function calcular() {
+//     let numero = parseInt(document.getElementById('numero').value)
+    
+//     for (i = 0; i <= 10; i++) {
+//         console.log(`A tabuada do ${numero} é:
+//         ${numero} x ${i} é igual a ${numero*i}`)
+        
+    
+//     }
+// }
+
+for (i = 100; i >= 0; i--) {
+    console.log(i)
+}
+
