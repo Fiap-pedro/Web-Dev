a906329355b8579d38e5b350e7be097ffe96cd40
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-08-14 21:27:34 -0300

    a

--- meu-site/src/App.jsx
+++ meu-site/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import InfosPessoais from './componentes/InfosPessoais'
 
 function App() {
   const [count, setCount] = useState(0)
--- meu-site/src/componentes/InfosPessoais.jsx
+++ meu-site/src/componentes/InfosPessoais.jsx
@@ -1,5 +1,15 @@
 function infosPessoais() {
+
+    const nome = 'Pedro'
+    const numero = '(11) 94224-3773'
+    const email = 'pedroshenriquepsantos@gmail.com'
+
+    return ( 
     <>
-        <h1>Informa</h1>
+        <h1>Informações pessoais</h1>
+        <p>Nome: {nome} </p>
+        <p>Contato: {numero} </p>
+        <p>Email: {email} </p>
     </>
+    )
 }
