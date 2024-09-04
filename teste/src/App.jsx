8bf4b73450ea81eb611db11955758ca19ad9fda3
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-09-04 20:54:08 -0300

    aula de css no react

---
+++ teste/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function App() {
+  return (
+   <>
+      <Header/>
+      <h1 className="text-primaryFiap text-5xl ml-20"> Titulo da página </h1>
+   </>
+  )
+}
+
+export default App
