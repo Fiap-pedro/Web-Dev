1434941c46c09c68f004f7ea9320a9a9e69dc915
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-09-11 21:25:52 -0300

    rotas dinamicas

---
+++ Portal-filmes-main/src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import MovieByGenrePage from './pages/MoviesByGenrePage.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MovieListPage from './pages/MovieListPage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import GenreListPage from "./pages/GenreListPage.jsx"
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <App/>,
+
+  children: [
+    {index: true, element: <Home/>},
+    {path: "movies", element: <MovieListPage/>},
+    {path: "movies/:id", element: <MovieDetailPage/>},
+    {path: "genre", element: <MovieByGenrePage/>},
+    {path: "genre/:id", element: <GenreListPage/>},
+    {path: "*", element: <PageNotFound/>}
+    
+  ]
+
+}
+  
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ Portal-filmes-main/src/pages/MoviesByGenrePage.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+export default function MoviesByGenrePage(){
+    return(
+        <>
+        <h1>Movies By Genre Page</h1>
+        {/*Lista todos os filmes do gênero selecionado pelo usuário. */}
+        </>
+    )
+}
