76bf9edfbbfbf60bdda1efff9101576d36d4613f
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-10-09 21:20:40 -0300

    integrando api no react usando promessas e useEffect

--- Portal-filmes-main/src/pages/MovieListPage.jsx
+++ Portal-filmes-main/src/pages/MovieListPage.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react"
-import movies from "../data/movies.json"
-import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from "react"
+
 
 export default function MovieListPage(){
 
     const [search, setSearch] = useState("")
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=c360aba74614c6e9141db60a0c2102e7&language=pt-BR')
+            .then(res => res.json())
+            .then(res => setFilmes(res.results))
+            .catch(erro => console.log(erro))
+            .finally(() => console.log('Fim!'))
+    }, []) //array de dependências
+
 
     // e = event
-    
+
     const handleSearch = (e) => {
             setSearch(e.target.value)
             console.log(search)
     }
 
-    const filmesFiltrados = movies.filter( filme => (
-        filme.titulo.toLowerCase().includes(search.toLowerCase())
-    ))
+    
 
     return(
         <>
@@ -30,16 +37,14 @@ export default function MovieListPage(){
 
             <section className="flex">
                 {
-                    filmesFiltrados.length > 0 ?
-
-                        filmesFiltrados
-                        .map(filme => (
-                            <MovieCard key={filme.id} {...filme}/>
-                        ))
-
-                        :
-                        
-                        <p>Não existem filmes na busca.</p>
+                   filmes.map( filme => (
+                    <>
+                        <h1>{filme.title}</h1>
+                        <img src={`https://image.tmdb.org/t/p/w92${filme.poster_path}`}/>
+                        <img src={`https://image.tmdb.org/t/p/w1280${filme.backdrop_path}`}/>
+                    </>
+                    
+                   ))
                 }
             </section>
         </>
